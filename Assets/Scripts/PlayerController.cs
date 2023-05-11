4363bbfcd458bf32ee96f5096c7faf4b8b5d4dc9
TotiFlame <santifer4321@gmail.com>
2023-05-11 18:42:03 -0300

    Task 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if ( Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow) )
+        {
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+        else if ( Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow) )
+        {
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+        else if ( Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+        else if ( Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        }
+    }
+}
